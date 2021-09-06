31358021accb5e3ddd34ff9173b183b2aeeed006
zeke-john <zekejohn118@gmail.com>
2021-09-06 15:50:07 -0700

    saving

--- New-Todo-App/app.py
+++ New-Todo-App/app.py
@@ -36,7 +36,6 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean)
     description = db.Column(db.String(150))
     start = db.Column(db.String(150))
-    
 
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
@@ -107,30 +106,69 @@ def edit(todo_id):
             form=form, 
             name_to_update=name_to_update)
 
+class New_Todo(db.Model):
+    new_id = db.Column(db.Integer, primary_key=True)
+    new_name = db.Column(db.String(50))
+    new_complete = db.Column(db.Boolean)
+    new_description = db.Column(db.String(150))
+    new_start = db.Column(db.String(150))
+
 @app.route("/calendar", methods=["GET", "POST"])
 def calendar():
     return render_template("calendar.html")
 
-
 @app.route('/calendar/<userInfo>/<daysInMonth>/<userMonth>', methods=['POST', 'GET'])
-def processUserInfo(userInfo, daysInMonth, userMonth):
-    days = json.loads(daysInMonth)
-    days = int(days)
+def calendarDay(userInfo, daysInMonth, userMonth):
+    new_todo_list = New_Todo.query.all()
+
+    daysInMonth = json.loads(daysInMonth)
+    daysInMonth = int(daysInMonth)
+
     userInfo = json.loads(userInfo)
-    if userInfo.isnumeric() == False:
-        return render_template("404.html")
     userInfo = int(userInfo)
-    
+
     userMonth = json.loads(userMonth)
     userMonth = int(userMonth)
 
-    if userInfo > days:
+    if userInfo > daysInMonth:
         return render_template("404.html")
     if userInfo < 1:
         return render_template("404.html")
 
-    return render_template('calendarDay.html')
+    return render_template('calendarDay.html', new_todo_list=new_todo_list, daysInMonth=daysInMonth , userInfo=userInfo, userMonth=userMonth)
 
+
+@app.route("/calendar/<userInfo>/<daysInMonth>/<userMonth>/clear")
+def new_clear(userInfo, daysInMonth, userMonth):
+    new_todo_list = New_Todo.query.all()
+    new_todo_list[:] = []
+    db.session.query(New_Todo).delete()
+    db.session.commit()
+    return redirect(url_for('calendarDay', userInfo=userInfo, daysInMonth=daysInMonth, userMonth=userMonth))
+
+@app.route("/calendar/<userInfo>/<daysInMonth>/<userMonth>/add", methods=["POST"])
+def new_add(userInfo, daysInMonth, userMonth):
+    new_name = request.form.get("new_name")
+    new_description = request.form.get("new_description")
+    new_start = request.form.get("new_start")
+    new_new_todo = New_Todo(new_name=new_name, new_complete=False, new_description=new_description, new_start=new_start)
+    db.session.add(new_new_todo)
+    db.session.commit()
+    return redirect(url_for('calendarDay', userInfo=userInfo, daysInMonth=daysInMonth, userMonth=userMonth))
+
+@app.route("/calendar/<userInfo>/<daysInMonth>/<userMonth>/update/<int:new_id>")
+def new_update(new_id, userInfo, daysInMonth, userMonth):
+    new_todo = New_Todo.query.filter_by(new_id=new_id).first()
+    new_todo.new_complete = not new_todo.new_complete
+    db.session.commit()
+    return redirect(url_for('calendarDay', userInfo=userInfo, daysInMonth=daysInMonth, userMonth=userMonth))
+
+@app.route("/calendar/<userInfo>/<daysInMonth>/<userMonth>/delete/<int:new_id>")
+def new_delete(new_id, userInfo, daysInMonth, userMonth):
+    new_todo = New_Todo.query.filter_by(new_id=new_id).first()
+    db.session.delete(new_todo)
+    db.session.commit()
+    return redirect(url_for('calendarDay', userInfo=userInfo, daysInMonth=daysInMonth, userMonth=userMonth))
 
 @app.errorhandler(404)
 def page_not_found(e):
